4c7077f197006462ff6213ca8b4e7299ed5de89c
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2020-11-26 11:43:14 +0900

    Update aggr_csv.py

--- aggr_csv.py
+++ aggr_csv.py
@@ -4,7 +4,7 @@ fpout=open("aggr_N.csv","w")
 fpout.write("library_id"+","+"molecule_h5"+","+"Status""\n")
 fpout1=open("aggr_T.csv","w")
 fpout1.write("library_id"+","+"molecule_h5"+","+"Status""\n")
-path='/home/hykim/Project/Cancer/Colon/scRNA-seq/FASTQ/count/'
+path='/Cancer/Colon/scRNA-seq/FASTQ/count/'
 file_list=os.listdir(path)
 sample=[file for file in file_list if file.startswith('PM-PS')]
 for i in sample:
