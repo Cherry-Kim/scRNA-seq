3c4533d1c32c5b410243e407a6f920e4b0f92028
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2021-02-03 18:30:54 +0900

    Update 1.scRNA-seq.py

--- 1.scRNA-seq.py
+++ 1.scRNA-seq.py
@@ -4,8 +4,8 @@ from rpy2 import robjects as ro
 r = ro.r
 
 def STEP1_COUNT(ref):
-	file_list=os.listdir('/home/hykim/Project/Cancer/Colon/scRNA-seq/FASTQ/FASTQ/')
-	a=[file for file in file_list if file.endswith('PM-PS')]
+	file_list=os.listdir('/home/hykim/FASTQ/')
+	a=[file for file in file_list if file.endswith('PS')]
 	for i in a:
        	 os.system('/home/hykim/SC/cellranger-4.0.0/cellranger count --id='+i+' --sample='+i+' --fastqs='+path+i+' --jobmode=local --transcriptome='+ref)
 
@@ -26,8 +26,8 @@ def MAKE_AGGRINPUT(path):
 	fpout1.close()
 
 def STEP2_AGGR():
-	os.system('/home/hykim/program/cellranger-4.0.0/bin/cellranger aggr --id=colon_N --csv=aggr_N.csv --normalize=mapped')
-	os.system('/home/hykim/program/cellranger-4.0.0/bin/cellranger aggr --id=colon_T --csv=aggr_T.csv --normalize=mapped')
+	os.system('/home/hykim/cellranger-4.0.0/bin/cellranger aggr --id=colon_N --csv=aggr_N.csv --normalize=mapped')
+	os.system('/home/hykim/cellranger-4.0.0/bin/cellranger aggr --id=colon_T --csv=aggr_T.csv --normalize=mapped')
 
 def STEP3_SEURAT():
 	r.source("2-1.seurat.r")
@@ -35,10 +35,10 @@ def STEP3_SEURAT():
 	r.source("2-2.seurat.r")
 
 def main():
-	ref='/home/hykim/SC/refdata-gex-GRCh38-2020-A'
-	path='/home/hykim/Project/Cancer/Colon/scRNA-seq/PAIR/'
-	#STEP1_COUNT(ref)
-	#MAKE_AGGRINPUT(path)
+	ref='/home/hykim/refdata-gex-GRCh38-2020-A'
+	path='/home/hykim/'
+	STEP1_COUNT(ref)
+	MAKE_AGGRINPUT(path)
 	STEP2_AGGR()
 	STEP3_SEURAT()
 main()
