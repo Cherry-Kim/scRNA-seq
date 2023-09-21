bd00ef82f16541a1d3bf26fc35c293275342e290
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2023-09-20 20:08:07 -0700

    Update 2.seurat_scCATCH.R

--- 2.seurat_scCATCH.R
+++ 2.seurat_scCATCH.R
@@ -4,12 +4,8 @@ barcode.path <- paste0(matrix_dir, "barcodes.tsv.gz")
 features.path <- paste0(matrix_dir, "features.tsv.gz")
 matrix.path <- paste0(matrix_dir, "matrix.mtx.gz")
 mat <- readMM(file = matrix.path)
-feature.names = read.delim(features.path, 
-                           header = FALSE,
-                           stringsAsFactors = FALSE)
-barcode.names = read.delim(barcode.path, 
-                           header = FALSE,
-                           stringsAsFactors = FALSE)
+feature.names = read.delim(features.path, header = FALSE, stringsAsFactors = FALSE)
+barcode.names = read.delim(barcode.path, header = FALSE, stringsAsFactors = FALSE)
 colnames(mat) = barcode.names$V1
 rownames(mat) = feature.names$V1
 #################################################
