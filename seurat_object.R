0c285b8359004842bc3733c5e045ac88425b9dc8
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2024-02-16 11:58:32 -0800

    Update seurat_object.R

--- seurat_object.R
+++ seurat_object.R
@@ -1,3 +1,4 @@
+Step1_input <- function(){ 
         library(sceasy)
         library(reticulate)
         library(dplyr)
@@ -38,3 +39,48 @@
         writeMM(obj = mat, file="matrix.mtx")
         write(x = rownames(subset_counts.data), file = "genes.tsv")
         write(x = colnames(subset_counts.data), file = "barcodes.tsv"
+}
+
+STEP2_seurat <- function(){
+        pbmc.data <- Read10X(data.dir = "/BIO3/STEP3_Seurat/", gene.column=1)
+        pbmc <- CreateSeuratObject(counts = pbmc.data)
+
+        pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = '^MT-')
+        # FeatureScatter can be used for anything calculated by the object, i.e. columns in object metadata, PC scores etc.
+        plot1 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "percent.mt")+ theme(legend.position = "none")
+        plot2 <- FeatureScatter(pbmc, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")+ theme(legend.position = "none")
+        plot1 + plot2
+        ## seu.obj.filtered <- subset(seu.obj, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & mitopercent < 5)
+
+        #Step2. Normalizing the data
+        pbmc <- NormalizeData(pbmc)
+
+        # Identification of highly variable features (feature selection)
+        pbmc <- FindVariableFeatures(pbmc)
+
+        #Scaling the data
+        all.genes <- rownames(pbmc)
+        pbmc <- ScaleData(pbmc, features = all.genes)
+
+        #Perform linear dimensional reduction
+        pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
+        ElbowPlot(pbmc)
+
+        #Cluster the cells
+        pbmc <- FindNeighbors(pbmc)
+        pbmc <- FindClusters(pbmc)      #resolution = 0.5
+        head(Idents(pbmc), 5)
+
+        #Run non-linear dimensional reduction (UMAP/tSNE)
+        pbmc <- RunUMAP(pbmc, dims = 1:15) #pbmc <- RunTSNE(pbmc, dims = 1:8)   
+        DimPlot(pbmc, reduction = 'umap')
+
+        #Finding differentially expressed features (cluster biomarkers)
+        pbmc.markers <- FindAllMarkers(pbmc, only.pos = TRUE)
+        pbmc.markers %>%
+                group_by(cluster) %>%
+                dplyr::filter(avg_log2FC > 1)
+
+        write.csv(pbmc.markers, "markers.csv", quote=F)
+}
+
